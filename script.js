e7dbbcf49c4b8c1db0bc9f83e97bc8b7296b225c
ausdotsn50 <ausdotsn50@gmail.com>
2025-12-09 07:47:20 +0800

    feat: div color on hover added

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
+// Let size be between 1 to 100 only
 // Create a webpage with a 16x16 grid of square divs.
-const width = 700;
-const height = 500;
+const width = 960;
+const height = 760;
 
 const container = document.querySelector("#container");
-const size = 20;
+const size = 2;
 const divWidth = width/size, divHeight = height/size;
 
 container.style.setProperty('--grid-width', width + 'px');
 container.style.setProperty('--grid-height', height + 'px');
+
+function handleEvent(event) {
+  if (event.type === "mouseenter") {
+    /* handle a full screen toggle */
+    event.target.style.setProperty('--grid-color', 'black');
+  } else {
+    /* handle a full screen toggle error */
+    console.log('he')
+  }
+}
 
 for(let i = 0; i < size; i++) { //row
     const outerDiv = document.createElement("div");
@@ -16,8 +27,9 @@ for(let i = 0; i < size; i++) { //row
 
     for(let j = 0; j < size; j++) { //col
         const innerDiv = document.createElement("div");
-        innerDiv.textContent = "o";
+        //innerDiv.textContent = "o";
         innerDiv.classList.add("inner")
+        innerDiv.addEventListener('mouseenter', handleEvent);
         outerDiv.appendChild(innerDiv);
     }
 }
